144ebbb87495d2ff75c1e722a5a4b215029d111b
Diogo Lucas de Oliveira <dluks82@gmail.com>
2023-07-18 17:09:25 -0300

    module name

---
+++ fundamentals_projects/area_circ_v6.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from math import pi
+
+radius = input('Enter the radius of your circle (m): ')
+
+print('Circ area:', pi * float(radius) ** 2)
+
+print('Module name:', __name__)
+# print('Package name:', __package__)
