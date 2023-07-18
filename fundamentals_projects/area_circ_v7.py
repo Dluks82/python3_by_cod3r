3602abda148d4eb531db83d5bdf56fbb5e9838b5
Diogo Lucas de Oliveira <dluks82@gmail.com>
2023-07-18 17:21:43 -0300

    first if

---
+++ fundamentals_projects/area_circ_v7.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+from math import pi
+
+if __name__ == '__main__':
+    radius = input('Enter the radius of your circle (m): ')
+
+    print('Circ area:', pi * float(radius) ** 2)
