6f8ce5ac44a885bb85985a6e269746847994f9f1
Diogo Lucas de Oliveira <dluks82@gmail.com>
2023-07-17 23:54:17 -0300

    import Decimals

--- notes/fundamentals.py
+++ notes/fundamentals.py
@@ -1,5 +1,7 @@
 # First examples
 
+from decimal import Decimal, getcontext
+
 print('First Program...')
 print(1 + 2)
 print(1 /
@@ -294,3 +296,37 @@ print(type(2+0))
 # print(type(2+'3')) # TypeError: unsupported operand type(s) for +: 'int' and 'str'
 print(2*'3')
 print(type(2*'3'))
+
+# Numeric types
+
+print(dir(int))
+print(dir(float))
+
+a = 7
+b = 3.2
+
+print(type(a))
+print(type(b))
+print(type(a+b))
+
+print(b.is_integer())
+print(5.0.is_integer())
+
+print(int.__add__(2, 3))
+print((-2).__abs__())
+
+print((-3.6).__abs__())
+
+# Decimal
+
+print(1.1 + 2.2)
+
+
+getcontext().prec = 10
+
+print(Decimal(1.1) + Decimal(2.2))
+print(Decimal(1) / Decimal(7))
+
+print(Decimal(1) / Decimal(7))
+
+print(Decimal.max(Decimal(1), Decimal(7)))
