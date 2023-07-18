ee9049f871ead3ea9e313cfbd4565a4165d40fa4
Diogo Lucas de Oliveira <dluks82@gmail.com>
2023-07-18 16:58:09 -0300

    v2-v5

---
+++ fundamentals_projects/area_circ_v3.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+pi = 3.1415
+radius = 15
+print('Circ area:', pi * radius ** 2)
---
+++ fundamentals_projects/area_circ_v4.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# import math
+from math import pi
+
+print(dir())
+
+print('Circ area:', pi * 15 ** 2)
