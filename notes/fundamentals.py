0e57298457048613f5a6cbde595b8b25058ae822
Diogo Lucas de Oliveira <dluks82@gmail.com>
2023-07-17 22:06:31 -0300

    basic types

---
+++ notes/fundamentals.py
@@ -0,0 +1,19 @@
+# First examples
+
+print('First Program...')
+print(1 + 2)
+print(1 /
+      + 2)
+
+# Basic types
+
+print(True)
+print(False)
+print(1.2+1)
+print('String')
+print("String")
+print('You are ' + 3 * 'very ' + 'cool')
+# print(3 + '3') TypeError: unsupported operand type(s) for +: 'int' and 'str'
+print([1, 2, 3])
+print({'name': 'Diogo', 'age': 41})
+print(None)
