12d5cf26ff8bd29154c49612b6f2abd5a3fa84a4
Diogo Lucas de Oliveira <dluks82@gmail.com>
2023-07-18 23:50:36 -0300

    validate number

---
+++ fundamentals_projects/student_grade.py
@@ -0,0 +1,12 @@
+grade = 2
+
+if grade >= 9:
+    print("Great job!")
+elif grade >= 7:
+    print("Good job!")
+elif grade >= 5:
+    print("Not bad.")
+elif grade >= 3:
+    print("You can do better.")
+else:
+    print("You failed.")
