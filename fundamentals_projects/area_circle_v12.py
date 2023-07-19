a15ca0118ddc0249a991ce6c9ef14886ed4fe31d
Diogo Lucas de Oliveira <dluks82@gmail.com>
2023-07-18 21:35:07 -0300

    help function

---
+++ fundamentals_projects/area_circle_v12.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+from math import pi
+import sys
+
+
+def help():
+    print('You must pass the radius of the circle.')
+    # sys.argv[0] is the name of the script
+    print(f'Syntax: {sys.argv[0]} <radius>')
+
+
+def circle(radius):
+    return pi * float(radius) ** 2
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        help()
+    else:
+        radius = sys.argv[1]
+        area = circle(radius)
+        print(f'The area of your circle is {area} m^2.')
