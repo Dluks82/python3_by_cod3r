e358230b83aac8a275ab8e2fa2084f2eb8390e9d
Diogo Lucas de Oliveira <dluks82@gmail.com>
2023-07-20 14:43:26 -0300

    read file

---
+++ file_handle/io_1.py
@@ -0,0 +1,10 @@
+#! /usr/bin/python3
+
+file = open('people.csv')
+data = file.read()
+
+file.close()
+
+for record in data.splitlines():
+    print('Name: {}, Age: {}'.format(
+        *record.split(',')))
---
+++ file_handle/io_3.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+
+file = open('people.csv')
+for record in file:
+    print('Name: {}, Age: {}'.format(*record.split(',')))
+
+file.close()
