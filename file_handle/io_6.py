cd03f918ef7cc3a15eecf59a203f5d7bf465171a
Diogo Lucas de Oliveira <dluks82@gmail.com>
2023-07-20 15:03:24 -0300

    write file

---
+++ file_handle/io_6.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python3
+
+with open('people.csv') as file:
+    with open('people.txt', 'w') as output:
+
+        for record in file:
+            person = record.strip().split(',')
+
+            print('Name: {}, Age: {}'.format(*person), file=output)
+
+if file.closed:
+    print('File is closed')
