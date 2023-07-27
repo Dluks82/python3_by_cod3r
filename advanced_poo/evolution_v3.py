27cc5d6b010e8bfb7666d5869cb87fd779990e23
Diogo Lucas de Oliveira <dluks82@gmail.com>
2023-07-27 13:48:48 -0300

    get set

---
+++ advanced_poo/evolution_v3.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+class Human:
+    # class attribute
+    specie = 'Homo Sapiens'
+
+    def __init__(self, name):
+        self.name = name
+        self._age = None
+
+    def get_age(self):
+        return self._age
+
+    def set_age(self, age):
+        if age < 0:
+            raise ValueError('Age must be a positive value.')
+        self._age = age
+
+    def from_the_caves(self):
+        self.species = 'Homo Neanderthalensis'
+        return self
+
+    @staticmethod
+    def species():
+        adjectives = ('Habilis', 'Erectus', 'Neanderthalensis', 'Sapiens')
+        return ('Australopiteco',) + tuple(f'Homo {adj}' for adj in adjectives)
+
+    @classmethod
+    def is_evolved(cls):
+        return cls.specie == cls.species()[-1]
+
+
+class Neanderthal(Human):
+    specie = Human.species()[-2]
+
+
+class HomoSapiens(Human):
+    specie = Human.species()[-1]
+
+
+if __name__ == '__main__':
+    jose = HomoSapiens('José')
+    # jose.set_age(-10)
+    jose.set_age(41)
+    print(f'Name: {jose.name} Age: {jose.get_age()}')
